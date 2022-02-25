6674908fdcdd23523f10319e62f552676ccc9f52
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 09:37:06 -0300

    Bracket Notation to Find lest Character in String

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Example
 var firstName= "Ada";
-var secundLetterOfFirstName= firstName[1];
+var secundLetterOfFirstName= firstName[firstName.length - 1];
 
 // Setup
 var lastName = "lovelace";
 
 // Only change code below this line.
-var thirdLetterOflastName = lastName[2];
+var thirdLetterOflastName = lastName[lastName.length -1];
