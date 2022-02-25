fe47eff6f984102a68f266649ff834870273950b
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 18:36:58 -0300

    Counting Cards

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-// Setup
-function abTest(a, b) {
-    
-    
-    if (a < 0|| b < 0) {
-        return undefined;
+var count = 0;
+
+function cc(card) {
+    switch(card){
+       case 2:
+       case 3:  
+       case 4: 
+       case 5: 
+       case 6:
+         count++;
+         break;
+       case 10:
+       case "J":  
+       case "Q":
+       case "K":
+       case "A":
+          count--;
+          break;
     }
+  
+    var holdbet = 'Hold'
+    if (count > 0){
+        holdbet = 'Bet'
+    }
+  
+    return count + " " + holdbet;
     
-    
-    // Only change code above this line
-    
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-// Change values below to test your code
-console.log(abTest(-2,2));
+cc(2); cc('K'); cc(10); cc('k'); cc('A');
+console.log(cc(7));
