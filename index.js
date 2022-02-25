b3296723407568e126d5b79fbb1df3f59789be54
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 18:05:11 -0300

    Replacing If Else Chains with Switch

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-function sequentialSizes(val) {
+function chainToS(val) {
     var answer = "";
-    switch(val) {
-        case 1:
-        case 2:
-        case 3:
-            answer = "Low";
+    // Only change code below this line
+    
+    switch(val){
+        case "bob":
+            answer = "Marley";
             break;
-        case 4:
-        case 5:
-        case 6:
-            answer = "Mid";
+        case 42:
+            answer = "The  Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much!";
             break;
         case 7:
-        case 8:
-        case 9:
-            answer = "High";
+            answer = "Ate Nine";
             break;
     }
+
+    // Only change code above this line
     return answer;
 }
-// Change this value to test
-console.log(sequentialSizes(8));
- 
