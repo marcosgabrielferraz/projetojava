a7b4e4ac16c361563fded124e15c8a68a467c2da
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 16:44:35 -0300

    Chaining If Else Atetements

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-function ordeMyLogic(val) {
-    if ( val < 5) { 
-      return "Less than 5";
-    } else if ( val < 10) { 
-      return "Less than 10";
-    } else{
-      return "Greater than or equal to 10";
+function testSize(num) {
+    if (num < 5){ 
+        return "Tiny"
+    } else if (num < 10) {
+        return "Small"
+    } else if (num < 15) {
+         return "Medium"   
+    } else if (num < 20) {
+        return "Large"
+    } else {
+        return "huge"
     }
 }
 
-// Change this value to test
-console.log(ordeMyLogic(3));
+console.log(testSize(19));
+
+/*
+White chained if/else if statements to fulfill the following conditions:
+
+num < 5 - return "Tiny"
+num < 10 - return "Small"
+num < 15 - return "Medium"
+num < 20 - return "Large"
+num >= 20 - return "huge"
+*/
