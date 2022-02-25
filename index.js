017be0c59c85d901b58b035ec093eae913ff23b4
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 15:27:45 -0300

    Comparison with the Greater Than Or Equal To Operator

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function testGreaterThan(val) {
-    if (val > 100) { // Change this line
-        return "Over 100";
+function testGreaterOrEqual(val) {
+    if (val >= 20) { // Change this line
+        return "20 or Over";
     }
     
-    if (val > 10) { // Change this line 
-        return "Over 10";
+    if (val >= 10) { // Change this line 
+        return "10 or Over";
     }
     
-    return "10 or Under";
+    return "Less than 10";
 }
 
 //Change this value to test
-console.log(testGreaterThan(10))
+console.log(testGreaterOrEgual(10))
