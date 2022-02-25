ebb2af1156e51ee96ce5b244b61f480aa5733541
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 16:19:03 -0300

    Else If Statements

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-function testElse(val) {
-    var result = "";
-    // Only change code below this line
-    
-    if ( val > 5) { 
-        result = "Bigger than 5";
-    } else {
-        result = "5 or Smaller";
+function testElseIf(val) {
+    if ( val > 10) { 
+      return "Greater than 10";
+    } else if ( val < 5) { 
+      return "Smaller than 5";
+    } else{
+      return "Between 5 and 10";
     }
     
-     // Only change code above this line      
-     return result;
+    
 }
 
 // Change this value to test
-testElse(4);
+testElseIf(7);
