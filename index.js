1211e920aec53e156505002547296d53207549b3
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 11:17:03 -0300

    Unshift()

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Example 
 var ourArray = ["Stimpsin", "J", ["cat"]];
-var removedFromOurArray = ourArray.shift();
-// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J",["cat"]]
+ourArray.shift();// ourArray now equal ["J", "cat"]
+ourArray.unshift("Happpy");
+//ourArray now equal ["Happy", "J", "cat"]
 
 // Setup
 var myArray = [["John", 23], ["cat", 3]];
+myArray.shift();
 
-// Only change code below this line
-var removedFrommyArray = myArray.shift();
+//Only change code below this line.
+myArray.unshift(["Paul", 35]);
