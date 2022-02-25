7787561d6df0a753c9d3a1e561433bb3881ca88a
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 15:05:56 -0300

    Practice Comparing Different Values

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-//Setup 
-function testStrict(val) {
-    if (val === 7) { // Change this line
+// Setup
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
         return "Equal";
     }
-    return "Equal";
+    return "not Equal";
 }
 
 // Change this value to test
-testStrict(10);
-
-/*
-3 == 3
-3 == '3'
-*/
+console.log(compareEquality(10, "10"))
