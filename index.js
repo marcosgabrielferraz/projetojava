64eb5052ee35710682a1a8ee7daacd41d3bafc54
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 20:23:51 -0300

    Nested Objects

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-var mymusic = [
-    {
-        "artist ":"Billy Joel",
-        "title":"Piano Man",
-        "release_year":1973,
-        "formats":[
-            "DC",
-            "8T",
-            "LP"
-        ],
-        "gold": true
-    },
-    // Add record here
-    {
-        "artist":"Beau Carnes",
-        "title":"Cereal Man",
-        "release_your": 2003,
-        "formats": [
-            "YouTube video"
-        ]
+// Setup 
+var myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+    "outside": {
+        "trunk": "jack"
+        }
     }
-]
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"] ; // Change this line
+
+console.log(gloveBoxContents);
