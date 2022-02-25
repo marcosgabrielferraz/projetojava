32cc9e6a24b5cd6700155da616d7107bf08ea260
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 11:10:35 -0300

    Manipulate Arrays with shift()

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 // Example 
-var ourArray = [1,2,3];
-var removedFromOurArray = ourArray.pop();
-// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+var ourArray = ["Stimpsin", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J",["cat"]]
 
 // Setup
-var myArray = [["John", 23], ["cat", 2]];
+var myArray = [["John", 23], ["cat", 3]];
 
 // Only change code below this line
-var removedFrommyArray = myArray.pop();
-
-console.log(myArray)
-
+var removedFrommyArray = myArray.shift();
