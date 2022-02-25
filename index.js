3250d4e894a5cc0e8587d40a36f049f1b6411c76
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 10:51:53 -0300

    push()

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// Example 
+var ourArray = ["stimpson", "J", "cat"];
+ourArray.push(["happy","joy"]);
+// ourArray now eguals ["Stimpson", "j", "cat", ["happy", "joy"]]
+
 // Setup
-var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+var myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line.
-var myData = myArray[2][1];
-console.log(myData);
+myArray.push(["dog", 3])
