4f406db7eeab9834d3bba18706fdcefddc27e031
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 10:02:38 -0300

    Word Blanks

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function wordBlanks(myNoun, myVerb, myAdverb){
     // Your code below this line
     var result = "";
-    result += "The" + myAdjective + myNoun +myVerb + "to the store" + myAdverb;
+    result += "The" + " " + myAdjective + " " + myNoun + " " + myVerb + "to the store" + myAdverb;
     //Your code above this line
     return result;
 }
 
 // Change the words here to test your function
 console.log(wordBlank("dog", "big", "ran", "quickly" ));
+console.log(wordBlank("bike", "slow", "flew", "slowly" ));
