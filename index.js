5b5c914442687a73561d6d602e0892d9a60b28d8
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 12:13:22 -0300

    Local Scope

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-// Declare your  variable here
-var myGlobal = 10;
-
-functionfun1() {
-    // Assign 5 to oopsGlobal here 
-    oopsGlobal = 5;
+function myLocalScope(){
+    var myVar = 5;
+    console.log(myVar);
 }
+myLocalScope();
 
-// Only change code above this line 
-function fun2(){
-    var output ="";
-    if(typeof myglobal != "undefined"){
-        output += "myGlobal: " + myGlobal;
-    }
-    if (typeof oopsGlobal != "undefined"){
-        output += " oopsGlobal: " + ooppsGlobal;
-    }
-    console.log(output);
-}
-fin1();
-fin2();
