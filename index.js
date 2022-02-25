e1e3641765d73c55b993937a82bd86f9039427ea
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 10:31:07 -0300

    Access Multi-Dimensional Arrays

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// Example
-var ourArrray = [18,64,99];
-ourArray[1] = 45;// ourArray now equals [18,45,99];
-
-//setup
-var myArray = [18,64,99];
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
 
 // Only change code below this line.
-myArray[0] = 45;
-console.log(myArray)
+var myData = myArray[2][1];
+console.log(myData);
