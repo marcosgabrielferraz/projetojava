6eb6c9d24243406fe367f019d95fa637800f8fd2
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 10:10:07 -0300

    Nest Arrays

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Example
-var ourArray = ["John", 23];
+var ourArray = [["the universe", 42],["everything", 101010]];
 
 // Only change code below this line
-var myArray =["Quincy", 1];
+var myArray =[["Bulls, 23"], ["White Sox", 45]];
