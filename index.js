a324a47884112dfd6a11aded10fd79f882841194
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 18:10:53 -0300

    Returning Boolean Values from Functions

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-function chainToS(val) {
-    var answer = "";
-    // Only change code below this line
-    
-    switch(val){
-        case "bob":
-            answer = "Marley";
-            break;
-        case 42:
-            answer = "The  Answer";
-            break;
-        case 1:
-            answer = "There is no #1";
-            break;
-        case 99:
-            answer = "Missed me by this much!";
-            break;
-        case 7:
-            answer = "Ate Nine";
-            break;
-    }
-
-    // Only change code above this line
-    return answer;
+function isLess(a, b) {
+    //Fix this code
+    return a < b;
 }
+
+// Change these values to test
+console.log(isLess(20, 15));
