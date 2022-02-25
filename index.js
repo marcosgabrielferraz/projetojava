41a8f87c906fa741e1df0e9119876723c7ba96ca
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 12:55:58 -0300

    If Statements

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function welcomeToBooleans(){
-    return true;
+// Example
+function uorTrueOrFalse(inItTrue){
+    if (isIttrue){
+        return "Yes, it's true";
+    }
+    return "No, it's false";
 }
+
+function trueOrFalse(wasThatTrue){
+        if (wasThatTrue){
+            return"Yes, that was true";
+        }
+        return "No, that was false";
+    
+}
+
+console.log(trueOrFalse(true));
