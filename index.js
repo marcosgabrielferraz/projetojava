504e687bacf809d07b01e6020c9c60f3dd75b33f
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 11:29:19 -0300

    Shopping List

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-// Example 
-var ourArray = ["Stimpsin", "J", ["cat"]];
-ourArray.shift();// ourArray now equal ["J", "cat"]
-ourArray.unshift("Happpy");
-//ourArray now equal ["Happy", "J", "cat"]
-
-// Setup
-var myArray = [["John", 23], ["cat", 3]];
-myArray.shift();
-
-//Only change code below this line.
-myArray.unshift(["Paul", 35]);
+var myList = [["cereal", 3], ["milk", 2], ["bananas", 3], ["juice", 2], ["eggs", 5]]
