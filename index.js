b7b34690d0a1cae005c9ca6bcc0a832b4e6b3016
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 18:58:08 -0300

    Accessing Object Properties with Bracket Notation

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // seetup
 var testObj = {
-    "hat": "ballcap",
-    "shirt": "jersey",
-    "shoes": "cleats"
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
 };
 
 // Only change code below this line
 
-var hatValue = testObj.hat;     // Change this line
-var shirtValue = testObj.shirt;   // Change this line
+var entreeValue = testObj["an an entree"];    // Change this line
+var drinkValue = testObj['the drink'];   // Change this line
