218933b95d98e8c66cb820e8d4f01fece6d1d612
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 10:06:20 -0300

    Arrays

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-function wordBlanks(myNoun, myVerb, myAdverb){
-    // Your code below this line
-    var result = "";
-    result += "The" + " " + myAdjective + " " + myNoun + " " + myVerb + "to the store" + myAdverb;
-    //Your code above this line
-    return result;
-}
+// Example
+var ourArray = ["John", 23];
 
-// Change the words here to test your function
-console.log(wordBlank("dog", "big", "ran", "quickly" ));
-console.log(wordBlank("bike", "slow", "flew", "slowly" ));
+// Only change code below this line
+var myArray =["Quincy", 1];
