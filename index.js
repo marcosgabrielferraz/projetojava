81dc29cfd38b83d9d327268adb02f14b5d79f992
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-24 23:08:14 -0300

    Brack Notation

---
+++ index.js
@@ -0,0 +1,14 @@
+// Example
+var firstLetter0fFirstName = "";
+var firstName = "Ada";
+
+firstLetter0fFirstName = firstName[0];
+
+// Setup
+var  firstLetter0fLastName = "";
+var lastName = "lovelace";
+
+// Only change code below this line.
+
+firstLetter0fFirstName = lastName[0];
+console.log(firstLetter0fLastName)
