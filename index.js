0175ea801fc94882072aa10fefd091f2330b8317
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 12:18:12 -0300

    Global vs Local Scope in Functions

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function myLocalScope(){
-    var myVar = 5;
-    console.log(myVar);
-}
-myLocalScope();
+var outerWear = "T-Shirt";
 
+function myOutfit() {
+    var outerWear = "sweater";
+    
+    return outerWear;
+}
+
+console.log(myOutfit());
+console.log(outerWear);
