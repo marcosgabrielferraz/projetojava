a59a7243cf4970847d06cff28c47ffaf8641f3b0
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 12:44:57 -0300

    Boolean Values

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function nextInline(arr, item) {
-    //Yuor code here
-    arr.push(item);
-    return arr.shift();
+function welcomeToBooleans(){
+    return true;
 }
-
-var testArr = [1,2,3,4,5];
- 
- console.log("Before:"+ JSON.stringify(testArr));
- console.log(nextInline(testArr, 6));
- console.log("After: " + Json.stringify(testArr));
