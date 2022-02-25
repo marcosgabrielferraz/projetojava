4192dcb9ed6be9496753af675b18290f2ff9d330
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 15:39:23 -0300

    Comparison with the Less Than Or Equal To Operator

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function testLessThan(val) {
-    if (val < 25) { // Change this line
-        return "Under 25";
+function testLessOrEqual(val) {
+    if (val <= 12) { // Change this line
+        return "Smaller Than or Equal to 12";
     }
     
-    if (val < 55) { // Change this line 
-        return "Under 55";
+    if (val <= 24) { // Change this line 
+        return "Swalller Than or Equal to 24";
     }
     
-    return "55 or Over";
+    return "More Than 24";
 }
 
 //Change this value to test
-console.log(testLeesThan(10));
+console.log(testLeesOrEqual(10));
