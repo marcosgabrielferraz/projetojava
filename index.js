e544a24191637386e642b0771e6fee7c31f18c47
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-25 21:02:06 -0300

    Record Collection

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-// Setup 
-var myPlants = [
-    {
-        type: "flowers",
-        list:[
-            "rose",
-            "tulip",
-            "dandelion"
+// Setup
+var collerction = {
+    "2548": {
+        "album":"Slippery When Wet",
+        "artist":"Bon Jovi",
+        "tracks": [
+            "Let It Rock",
+            "You Give Love a Bad Name"
         ]
     },
-    {
-        type: "trees",
-        list: [
-            "fir",
-            "pine",
-            "birch"
+    "2468": {
+        "album":"1999",
+        "artist":"Prince,",
+        "tracks":[
+            "1999",
+            "Little Red Corvette"
         ]
+    },
+    "1245": {
+        "artist":"Robert Palmer",
+        "tracks": []
+    },
+    "5439": {
+        "album": "ABBA Gold"
     }
-];
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse (JSON.stringify(collerction));
 
 // Only change code below this line
+function updateRecornds(id, prop, value){
+    if (value === "") {
+        delete collerction [id][prop];
+    } else if (prop === "tratcks"){
+    collerction[id][prop] = collerction[id][prop]|| [];
+    collerction[id][prop].push(value);
+    } else {
+        collerction[id][prop] = value;
+    }
+    return collerction;
+}
 
-var secundtree =myPlants[1].list[1]; //Change this line
+//Alter values below to test your code
+updateRecornds(2468, "trachs", "test");
+console.log(updateRecornds(5439, "artist", "ABBA"));
