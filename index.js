2d92ee49efed0969a17472570890bbcc3b651d54
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-26 22:42:27 -0300

    Do...While Loops

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-function multiplyAll(arr) {
-    var product = 1;
-    
-    for (var i=0; i < arr.length; i++){
-        for (var j=0; j < arr[i].length; j++) {
-            product *= arr[i][j];
-        }
-    }
-    
-    return product;
-}
+// Setup
+var myArray = [];
+var i = 10;
 
-var product = multiplyAll([[1,2],[3,4],[5,6,7]]);
+// Only change code below this line.
 
-console.log(product);
+do {
+    myArray.push(i);
+    i++;
+} while (i < 5)
+
+console.log(i, myArray);
