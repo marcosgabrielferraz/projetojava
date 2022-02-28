3137bdf2045ea4b0e7165d069dc4ba637f252ffe
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 15:59:01 -0300

    2

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-// Declarations
-var studlyCapVar;
-var properCamelCase;
-var titlecaseOver;
+var sum = 10 + 10;
+console.log(sum)
 
-//Assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titlecaseOver = 9000;
+var difference= 45 - 33;
+console.log(difference)
+
+var product = 8 * 10;
+var quotient = 66 / 33;
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+var myVar = 87;
+
+// Only change code below this line
+myVar--;
+
+var ouDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal =0.009
+
+var product = 2.0 * 2.5;
+console.log(product)
+
+var quotient= 4.4 / 2.0;
+
+// Only change code below this line
+
+var remainder
+remainder= 11 % 3;
