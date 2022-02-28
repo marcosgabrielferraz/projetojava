50328333aad3ae1ec4374ca07ea53026f1e5f564
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 12:45:12 -0300

    Destruturing Assignment with Nested Objects

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-var voxel = {x: 3.6, y: 7.4, z: 6.54};
-
-var x = voxel.x; // x = 3.6
-var y = voxel.y; // y = 7.4 
-var z = voxel.z; // z = 6.54
-
-const { x : a, Y : b, z : c} = voxel // a = 3.6, b = 7.4, c = 6.54
-
-const AVG_TEMPERATURES = {
-    today: 77.5,
-    tomorrow: 79
+const LOCAL_FORECAST = {
+    today: {min: 72, max: 83},
+    tomorrow: { min: 73.3, max: 84.6}
 };
 
-function getTempOfTmrw(avgTemperatures){
+function getMaxOfTmrw(forecast) {
     "use strict";
-    // change code below this line
-    const { tomorrow : tempOfTomorrow } = avgTemperatures; // change this line
-    // change code above this line
-    return tempOfTomorrow;
+    
+    const { tomorrow : { max : maxOfTomorrow }} = forecast;
+    
+    return maxOfTomorrow;
 }
 
-
-console.log(getTempOfTmrw(AVG_TEMPERATURES)); // SHOULD BE 79
+console.log(getMaxOfTmrw(LOCAL_FORECAST));
