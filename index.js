f6023808ea1ff4ce08120b5091effd4d427cce96
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 15:35:20 -0300

    create an export fallback with export default

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const capitalizeString = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-export { capitalizeString };
-
-export const foo = "bar";
-export const bar = "foo";
+function subtract(x, y) {return x - y;}
