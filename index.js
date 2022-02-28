7b0e5906342e30ef0500e026dbb7b45c63f9df95
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 13:42:08 -0300

    Simple fields

--- index.js
+++ index.js
@@ -1,27 +1,2 @@
-console.log(greeting);
-
-
-
-
-const result = {
-    success: ["max-length", "no-amd", "prefer-arrow-functions"],
-    failure: ["no var", "var-on-top", "linebreak"],
-    skipped: ["id-blacklist", "no-dup-keys"]
-};
-function makeList(arr) {
-    const resultDisplayArray = [];
-    for (let i = 0; i < arr.length; i++) {
-        resultDisplayArray.push(`<li class="text-warning">${arr[i]}</li>`)
-    }
-    
-    return resultDisplayArray;
-}
-/**
- * makeList(result.failure) should return:
- * [ `<li class="text-warning">no-var</li>`,
- * `<li class="text-warning">var-on-top</li>`,
- * `<li class="text-warning">linebreak</li>`]
- **/
- const resultDisplayArray = makeList(result.failure);
- 
- console.log(resultDisplayArray);
+const createPerson = (name, age, gender) => ({ name, age, gender})
+console.log(createPerson("Zodiac Hasbro", 56, "male"));
