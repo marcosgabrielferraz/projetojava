12c279d5d40bf4aee02e32716a16175fb8e4e85c
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 08:42:52 -0300

    Generate Random Whole Numbers

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function randomFraction() {
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20); 
+
+
+function randomWholeNum() {
 
 
     return Math.random();
 }
 
-console.log(randomFraction());
+console.log(randomWholenum());
