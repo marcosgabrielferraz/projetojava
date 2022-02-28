2c8f76ee249793ab32ca2550e0b84c0df8d1ac11
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 11:46:10 -0300

    Write Higher Order Arrow Functions

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var myconcat = (arr1, arr2) => arr1.concat(arr2);
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 
-console.log(myconcat([1, 2], [3, 4, 5]));
+const squareList = (arr) => {
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+    return squaredIntegers;
+};
+
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
