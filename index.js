d664cb056319e1d5cf36589109a960341ffb2b9e
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 09:40:55 -0300

    Use the Conditional (Ternary) Operator

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function convertToInteger(atr){
-    returnparseInt(str, 2)
- }
- 
- convertToInteger("10011");
+// condition ? statement-if-true : statement-if-false;
+
+function chackEqual(a, b) {
+    return a === b ? true : false;
+    
+    return a === b;
+}
+
+chackEqual(1, 2);
