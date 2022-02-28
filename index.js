1172480520330dce07cd70b2bbae85b1ed91f555
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 11:51:42 -0300

    White Higher Order Arrow Functions

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
-
-const squareList = (arr) => {
-    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
-    return squaredIntegers;
-};
-
-
-const squaredIntegers = squareList(realNumberArray);
-console.log(squaredIntegers);
+const increment = (function(){
+    return function increment(number, value = 1) {
+        return number + value;
+    };
+})();
+console.log(increment(5, 2));
+console.log(increment(5));
