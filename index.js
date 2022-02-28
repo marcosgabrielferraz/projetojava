0de52e986ef68385abaa58963d3be0446c957f6f
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 07:57:08 -0300

    Generate Random Fractions

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
-// Setup 
-var contacts = [
-    {
-        "firsName":"Akira",
-        "lastName":"Laine",
-        "number":"0543236543",
-        "like":["Pizza", "Coding", "Brownie Points"] 
-        },
-    {
-        "firsName":"Harry",
-        "lastName":"Potter",
-        "number":"0994372684",
-        "like":["Hogwarts", "Magic", "Hagrid"] 
-        },
-    {
-        "firsName":"Sherlock",
-        "lastName":"Holmes",
-        "number":"0487345643",
-        "like":["Intriguing Cases", "violin"] 
-        },
-    {
-        "firsName":"kristian",
-        "lastName":"Vos",
-        "number":"unknown",
-        "like":["JavaScript", "Gaming", "Foxes"] 
-        }
-];
-
-function lookUpProfile(name, prop){
-    for (var i = 0; i < contacts.length; i++) {
-        if(contacts[i].firsName === name){
-            return contacts[i][prop] || "No such property";
-        }
-    }
-    return "No such contact";
+function randomFraction() {
+    
+    
+    return 0;
 }
 
-//Change hese values to test function
-var data = lookUpProfile("Shirlock", "lastName");
-
-console.log(data);
+console.log(randomFraction())
