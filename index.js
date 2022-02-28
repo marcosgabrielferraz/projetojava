eb21127ee2c129cf540ddcd8dec2fbb741829bed
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 12:03:14 -0300

    Use the Rest Operator with Function Parameters

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const increment = (function(){
-    return function increment(number, value = 1) {
-        return number + value;
+const sum = (function(){
+    return function sum(...args){
+        return args.reduce((a, b) => a + b, 0);
     };
 })();
-console.log(increment(5, 2));
-console.log(increment(5));
+console.log(sum(1, 2, 3, 4));
