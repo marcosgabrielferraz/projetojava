d46e2dcefb90c4ada32e5e1d911139a486aecf35
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 13:36:36 -0300

    Create Strings using Template Literals

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const stats = {
-    max: 56.78,
-    stadard_deviation: 4.34,
-    median: 34.54,
-    mode: 23.87,
-    min: -0.75,
-    average: 35.85
+console.log(greeting);
+
+
+
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no var", "var-on-top", "linebreak"],
+    skipped: ["id-blacklist", "no-dup-keys"]
 };
-const half = (function(){
+function makeList(arr) {
+    const resultDisplayArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        resultDisplayArray.push(`<li class="text-warning">${arr[i]}</li>`)
+    }
     
-    return function half({ max, min }){
-        return (max + min)/ 2.0;
-    };
-    
-})();
-console.log(stats);
-console.log(half(stats));
+    return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ * `<li class="text-warning">var-on-top</li>`,
+ * `<li class="text-warning">linebreak</li>`]
+ **/
+ const resultDisplayArray = makeList(result.failure);
+ 
+ console.log(resultDisplayArray);
