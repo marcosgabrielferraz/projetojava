9b46494df19c6f7377d5bd0639d9324af3f33262
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 11:27:20 -0300

    Use Arrow Functions to White Concise Anonymous Functions

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function freezeObj() {
-    "use strict";
-    const MATH_CONSTAANTS = {
-        PI:3.14
-    };
-    
-    try {
-        MATH_CONSTAANTS.PI = 99;
-    }   catch( ex ) {
-        console.log(ex);
-    }
-    return MATH_CONSTAANTS.PI;
-}
-
-const PI = freezeObj();
-
-console.log(PI);
+var magic = () => new Date();
