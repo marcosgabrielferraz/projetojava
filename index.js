6166618eff0cc2e5a95c1025cb7b39c80c236d4a
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 16:01:09 -0300

    4

--- index.js
+++ index.js
@@ -1,62 +1,19 @@
-// Example
-var firstName = "Alan";
-var lastName = "Turing";
-
-//Only change code below this line 
-
-var myFirstName = "Beau";
-var myLastName = "Carnes";
-
-// Example
-var ourStr = "I come first." + "I come second.";
-
-// Only change code below this line 
-
-var myStr = "This is the start." + "This is the end."
-console.log(myStr);
-
-// Example
-var ourStr = "I come first. ";
-ourStr += "I come second.";
-
-// Only change code below this line 
-
-var myStr = "This is the first sentence. "
-myStr += "This is the second sentence."
-
-console.log(myStr);
- 
-// Example
-var ourName = "freeCodeCamp";
-var ourStr = "Hello, our name is" + ourName + ", how are you?";
-
-// Only change code bellow this line
-var myName= "Beau";
-var myStr= "My name is " + myName + " and I am well!";
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"";
 console.log(myStr)
 
-/// Example
-var anAdjective = "awesome!";
-var ourStr = "freeCodeCamp is";
-ourStr += anAdjective
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
-// Onlty change code below this line
+/*****
+CODE OUTPUT
+\'  single quote
+\"  double quote
+\\  backslash
+\n  newline
+\r  carriage return
+\t  tab
+\b  backspace
+\f form feed 
+*****/
 
-var someAdjective;
-var myStr = "Learning to code is";
-myStr += someAdjective;
+var myStr = "Firstline\n\t\\SecondlLine\nThirdLine"
 
-// Example
-var firstNameLength = 0;
-var firstName = "Ada";
-
-firstNameLength = firstName.length;
-
-// Setup
-var lastNameLength = 0;
-var lastName = "lovelace";
-
-// Only change code below this line.
-
-lastNameLength = lastName.length;
-console.log(lastNameLength)
