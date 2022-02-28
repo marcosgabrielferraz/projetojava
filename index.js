c2cfc3c81f7f6e718d3046a0f6e35ebe7bd51bb6
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 16:01:49 -0300

    5

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-var myStr = "I am a \"double quoted\" string inside \"double quotes\"";
-console.log(myStr)
+// Setup
+var myStr = "Jello world";
 
-var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+// Only change code below this line 
 
-/*****
-CODE OUTPUT
-\'  single quote
-\"  double quote
-\\  backslash
-\n  newline
-\r  carriage return
-\t  tab
-\b  backspace
-\f form feed 
-*****/
-
-var myStr = "Firstline\n\t\\SecondlLine\nThirdLine"
-
+myStr[0] = "Hello World";
