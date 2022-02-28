0a4660d7e337af3c232ac242c08fda19606a6d2f
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 08:57:43 -0300

    Use the parseInt function

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-// Example
-function ourRandomRange(ourMin, ourMax) {
-    
-    return Math.floor(Math.random() * (ourMax - ourMin + 1))+ourMin;
+function convertToInteger(atr){
+    return parseInt(str);
 }
 
-ourRandomRange(1, 9);
-
-// Only change code below this line.
-
-function randomRange(myMin, myMax) {
-    
-    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
-    
-}
-
-var myRondom = randomRange(5, 15);
-
-console.log(myRondom);
+convertToInteger("56");
