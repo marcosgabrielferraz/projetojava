9fdb34782f873187ae5951835e822475316e13e0
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 15:23:33 -0300

    Import and Export

--- index.js
+++ index.js
@@ -1,35 +1,4 @@
-class book {
-    constructor(author) {
-        this._author = author;
-    }
-    // getter
-    get writer (){
-        return this._author;
-    }
-    //setter 
-    set writer (updatedAuthor){
-        this._author = updatedAuthor;
-    }
-}
+import {capitalizeString} from "./string_function"
+const cap = capitalizeString("Hello!");
 
-function makeClass(){
-    class Thermostat {
-        constructor(temp){
-            this._temp = 5/9 * (temp - 32);
-        }
-        get temperature(){
-            return this._temp;
-        }
-        set temperature(updatedTemp){
-            this._temp = updatedTemp;
-        }
-    }
-    return Thermostat;
-}
-
- const Thermostat = makeClass();
- const thermos = new Thermostat(76);
- let temp = thermos.temperature;
- thermos.temperature = 26;
- temp = thermos.temperature;
- console.log(temp)
+console.log(cap);
