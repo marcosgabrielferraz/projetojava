d43fb9dc82b5c481b9b61c691cfc3d5a12ba0e1c
marcos12356 <marcosgabrieldasilvaferraz@gmail.com>
2022-02-28 15:51:19 -0300

    Initializing Variables with th Assignment Operator 2

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-var a = 3;
-var b = 17;
-var c = 12;
+//Initilize these three variable
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + "String!";
+
+var a = 5;
+var b = 12;
+var c = 4.6;
 
 //Only modify code below this line 
 
-a += 12;
-b += 9;
-c += 7;
+a *= 5;
+b *= 3;
+c *= 10;
+
+var a = 48;
+var b = 108;
+var c = 33,
+
+// Only modify code below this line
+
+a /= 12;
+b /=  4;
+c /= 11;
